78f63b3b15902a5f0d94e89d61249db34537a2d8
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-08-17 08:07:12 -0600

    Adding validation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+var invalidemails = "info@jubeljahr2000.de,office@kath.jungschar.atder,info@jubeljahr2000.de";
 function extractEmails (text) {
     return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
 }
@@ -6,10 +7,53 @@ function getEmailsFromText(text) {
     var emails = extractEmails(text),
         emailList = [];
     $.each(emails, function(index, email){
-        emailList.push(email.trim().toLowerCase());
+        if(invalidemails.indexOf(email) === -1) {
+            validateEmails(email);
+            emailList.push(email.trim().toLowerCase());
+        }
     });
 
-    return emailList.join(",");
+    return emailList.join("\n");
+}
+
+function validateEmails(email) {
+    //pubkey-
+    var countFalse = 0;
+    $.ajax({
+        type: "GET",
+        url: 'https://api.mailgun.net/v2/address/validate?callback=?',
+        data: { 
+            address: email, 
+            api_key: 'pubkey-63824d07bccc68fce2564ac14ace9b93' 
+        },
+        async: false,
+        dataType: "jsonp",
+        crossDomain: true,
+        success: function(data, status_text) {
+            //clearTimeout(timeoutID);
+
+            //element.mailgunLastSuccessReturn = data;
+            //if (options && options.success) {
+                //options.success(data, options.e);
+            //}
+            if(!data.is_valid) {
+                countFalse++;
+                console.log(data.address);
+            }
+        },
+        error: function(request, status_text, error) {
+            /*clearTimeout(timeoutID);
+            error_message = 'Error occurred, unable to validate address.';
+
+            if (options && options.error) {
+                options.error(error_message, options.e);
+            }
+            else {
+                if (console) console.log(error_message);
+            }*/
+            console.log(error_message);
+        }
+    });
 }
 
 $(function () {
